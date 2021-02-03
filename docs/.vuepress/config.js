de5f4f83d71eac2348634cfaee1c7dda088a1f0b
Warren Anderson <warren@wyomingsoftware.com>
2021-02-03 08:38:17 -0700

    using docs-devkit theme and versioning

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: 'Just playing around',
   plugins: ['@vuepress/last-updated'],
   plugins: [
-
+    'versioning',
     ['@vuepress/search', {
       searchMaxSuggestions: 10
     }]
@@ -11,27 +11,102 @@ module.exports = {
   markdown: {
     lineNumbers: true
   },
+  theme: 'titanium',
   themeConfig: {
     logo: '/assets/img/logo2.png',
     nav: [
       { text: 'Requesters', link: '/requesters/' },
       { text: 'Providers', link: '/providers/' },
-      {
+      { text: 'Hackathon 2022', link: '/hackathon/' },
+      /*{
         text: 'Versions',
         ariaLabel: 'Versions Menu',
         items: [
           { text: '1.0.0', link: '/1.0.0/' },
           { text: '1.0.1', link: '/1.0.1/' }
         ]
-      },
+      },*/
       
       
       { text: 'GitHub', link: 'https://github.com/wkande/compare-vuepress' },
     ],
-    sidebar: [
+    sidebar:{
+      "/providers/": [
+        
+        {
+          "title": "Providers",
+          "collapsable": false,
+          "children": [
+            [
+              "",
+              "Introduction"
+            ],
+            '/providers/api-integrations',
+            '/providers/configure-airnode',
+            '/providers/deploy-airnode',
+            '/providers/set-authorizers'
+          ]
+        },
+        {title:"________________", "collapsable": false,},
+        {
+          "title": "More Guides",
+          "collapsable": false,
+          "children": [
+            '/requesters/',
+            '/hackathon/'
+          ]
+        },
+      ],
+      "/requesters/": [
+        {
+          "title": "Requesters",
+          "collapsable": false,
+          "children": [
+            [
+              "",
+              "Introduction"
+            ],
+            "/requesters/create-request",
+            "/requesters/develop-client-contract"
+          ]
+        },
+        {title:"________________", "collapsable": false,},
+        {
+          "title": "More Guides",
+          "collapsable": false,
+          "children": [
+            '/providers/',
+            '/hackathon/'
+          ]
+        },
+      ],
+      "/hackathon/": [
+        {
+          "title": "Hackathon 2020",
+          "collapsable": false,
+          "children": [
+            [
+              "",
+              "Introduction"
+            ],
+            "/hackathon/location"
+          ]
+        },
+        {title:"________________", "collapsable": false,},
+        {
+          "title": "More Guides",
+          "collapsable": false,
+          "children": [
+            '/requesters/',
+            '/providers/'
+          ]
+        },
+      ],
+    }
+    /*sidebar: [
       {
         title: 'Requesters',   // required
-        path: '/requesters/',      // optional, link of the title, which should be an absolute path and must exist
+        path: '',      // optional, link of the title, which should be an absolute path and must exist
         collapsable: true, // optional, defaults to true
         sidebarDepth: 1,    // optional, defaults to 1
         children: [
@@ -60,13 +135,7 @@ module.exports = {
           '/hackathon/location'
         ]
       },
-    ]
-      
-
-  
-
-
-
+    ]*/
     
   }
 }
